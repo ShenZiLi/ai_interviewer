52c0bf5d6fa0fbcd687ede118c1b234ca66ff2fd
ShenZiLi <zilishen@qq.com>
2026-09-18 17:07:13 +0800

    build: scaffold TypeScript monorepo

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import js from '@eslint/js';
+import prettier from 'eslint-config-prettier';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    ignores: ['**/dist/**', '**/coverage/**', '**/node_modules/**', 'prototype/**', 'schemas/**'],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  prettier,
+);
